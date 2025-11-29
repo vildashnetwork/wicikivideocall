31b32223798f9d11f0c26681ce7a2f0c7cfa47a7
Li Blissz <liblissz3@gmail.com>
2025-11-29 23:52:47 +0100

    Fix SocketProvider for Vercel SSR

--- src/context/SocketProvider.jsx
+++ src/context/SocketProvider.jsx
@@ -1,22 +1,51 @@
+// import React, { createContext, useContext, useMemo } from 'react'
+// import { io } from "socket.io-client";
+
+// const SocketContext = createContext(null);
+
+// export const useSocket = () => {
+//     const socket = useContext(SocketContext);
+//     return socket;
+// }
+
+// const SocketProvider = (props) => {
+//     //https://wicikibackendcall.onrender.com
+//     // const socket = useMemo(() => io("video-peers-server.onrender.com/"), []);
+//     const socket = useMemo(() => io("wicikibackendcall.onrender.com/"), []);
+//     return (
+//         <SocketContext.Provider value={socket}>
+//             {props.children}
+//         </SocketContext.Provider>
+//     )
+// }
+
+// export default SocketProvider
+
+
+
+
+
+
 import React, { createContext, useContext, useMemo } from 'react'
 import { io } from "socket.io-client";
 
 const SocketContext = createContext(null);
 
-export const useSocket = () => {
-    const socket = useContext(SocketContext);
-    return socket;
-}
+export const useSocket = () => useContext(SocketContext);
 
-const SocketProvider = (props) => {
-    //https://wicikibackendcall.onrender.com
-    // const socket = useMemo(() => io("video-peers-server.onrender.com/"), []);
-    const socket = useMemo(() => io("wicikibackendcall.onrender.com/"), []);
+const SocketProvider = ({ children }) => {
+    const socket = useMemo(() => {
+        if (typeof window !== "undefined") {
+            return io("https://wicikibackendcall.onrender.com/")
+        }
+        return null; // do not create socket on server
+    }, []);
+
     return (
         <SocketContext.Provider value={socket}>
-            {props.children}
+            {children}
         </SocketContext.Provider>
     )
 }
 
-export default SocketProvider
+export default SocketProvider;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,24 @@
-import SocketProvider from '@/context/SocketProvider'
+// import SocketProvider from '@/context/SocketProvider'
+// import '@/styles/globals.css'
+
+// export default function App({ Component, pageProps }) {
+//   return (
+//     <SocketProvider>
+//       <Component {...pageProps} />
+//     </SocketProvider>)
+// }
+
+// src/pages/_app.js
+import dynamic from 'next/dynamic'
 import '@/styles/globals.css'
+
+// Load SocketProvider only in browser
+const SocketProvider = dynamic(() => import('@/context/SocketProvider'), { ssr: false })
 
 export default function App({ Component, pageProps }) {
   return (
     <SocketProvider>
       <Component {...pageProps} />
-    </SocketProvider>)
+    </SocketProvider>
+  )
 }
